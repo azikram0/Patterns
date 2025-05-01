d4db0b68b2bc90d01b26f209768c2a42fe8691ac
Elizaveta <elisavetayapryntseva@yandex.ru>
2025-05-01 22:03:11 +0400

    Добавлены реализации методов transformNumber и transformBinaryOperation для того, чтобы CopySyntaxTree мог обойти и скопировать типы узлов: Number и BinaryOperation

--- src/task1/CopySyntaxTree.java
+++ src/task1/CopySyntaxTree.java
@@ -3,12 +3,14 @@ package task1;
 public class CopySyntaxTree implements Transformer {
     @Override
     public Expression transformNumber(Number number) {
-        return null;
+        return new Number(number.value());
     }
 
     @Override
     public Expression transformBinaryOperation(BinaryOperation binOp) {
-        return null;
+        Expression newLeft = binOp.left().transform(this);
+        Expression newRight = binOp.right().transform(this);
+        return new BinaryOperation(newLeft, binOp.operation(), newRight);
     }
 
     @Override
