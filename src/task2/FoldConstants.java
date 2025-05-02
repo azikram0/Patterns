75bece7a9ca5125136c23f3d5933cf322af6fe0b
Elizaveta <elisavetayapryntseva@yandex.ru>
2025-05-02 14:09:12 +0400

    Добавлена реализация метода transformFunctionCall для сворачивания (в Number) выражения узла типа FunctionCall
    Вызов функции подразумевает работу с вызовом sqrt (квадратный корень) и abs (модуль), аргументом может быть как выражение типа Number, так и более сложное, например, BinaryOperation, такие выражения также подходят под условия сворачивания. Сворачивание для данного случая происходит следующим образом:
    1) Если аргумент является числом, то вычисляется функция и возвращается результат
    2) Если аргумент не является числом, то возвращается выражение, которое можно вычислить позже (после того, как его аргумент будет вычислен)

--- src/task2/FoldConstants.java
+++ src/task2/FoldConstants.java
@@ -3,6 +3,9 @@ package task2;
 import task1.*;
 import task1.Number;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FoldConstants implements Transformer {
     @Override
     public Expression transformNumber(Number number) {
@@ -13,23 +16,44 @@ public class FoldConstants implements Transformer {
     public Expression transformBinaryOperation(BinaryOperation binOp) {
         Expression left = binOp.left().transform(this);
         Expression right = binOp.right().transform(this);
-        double l = ((Number) left).value();
-        double r = ((Number) right).value();
-        double result;
-        switch (binOp.operation()){
-            case '+': result = l + r; break;
-            case '-': result = l - r; break;
-            case '*': result = l * r; break;
-            case '/': result = l / r; break;
-            default:
-                throw new UnsupportedOperationException("Неизвестный оператор: " + (char) binOp.operation());
+        if (left instanceof Number && right instanceof Number) {
+            double l = ((Number) left).value();
+            double r = ((Number) right).value();
+            double result;
+            switch (binOp.operation()) {
+                case '+':
+                    result = l + r;
+                    break;
+                case '-':
+                    result = l - r;
+                    break;
+                case '*':
+                    result = l * r;
+                    break;
+                case '/':
+                    result = l / r;
+                    break;
+                default:
+                    throw new UnsupportedOperationException("Неизвестный оператор: " + (char) binOp.operation());
+            }
+            return new Number(result);
         }
-        return new Number(result);
+        return new BinaryOperation(left, binOp.operation(), right);
     }
 
     @Override
     public Expression transformFunctionCall(FunctionCall funcCall) {
-        return null;
+        Expression arg = funcCall.arg().transform(this);
+        if (arg instanceof Number){
+            double value = ((Number) arg).value();
+            switch (funcCall.name()){
+                case "sqrt": return new Number(Math.sqrt(value));
+                case "abs": return new Number(Math.abs(value));
+                default:
+                    throw new UnsupportedOperationException("Неподдерживаемая функция: " + funcCall.name());
+            }
+        }
+        return new FunctionCall(funcCall.name(), arg);
     }
 
     @Override
