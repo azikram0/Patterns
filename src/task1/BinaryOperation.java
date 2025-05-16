06e856e547c030401a97abff8a768dab02f58417
Elizaveta <elisavetayapryntseva@yandex.ru>
2025-05-16 18:54:10 +0400

    Добавление иерархии ошибок
    Встроенные типы ошибок часто слишком общие и не позволяют точно выразить природу возникшей проблемы. Например, использование стандартного Exception или RuntimeException не дает четкого понимания, что именно пошло не так. Собственная иерархия ошибок позволяет явно определять типы исключений, соответствующие конкретным сценариям в приложении

--- src/task1/BinaryOperation.java
+++ src/task1/BinaryOperation.java
@@ -1,18 +1,26 @@
 package task1;
 
+import exception.NullArgument;
+import exception.UnsupportedOperation;
+
 public class BinaryOperation extends Expression {
-    public static final int PLUS  = '+';
+    public static final int PLUS = '+';
     public static final int MINUS = '-';
-    public static final int DIV   = '/';
-    public static final int MUL   = '*';
+    public static final int DIV = '/';
+    public static final int MUL = '*';
 
     private final Expression left;
     private final Expression right;
     private final int op;
 
     public BinaryOperation(Expression left, int op, Expression right) {
-        if (left == null || right == null) {
-            throw new IllegalArgumentException("Операнды не должны быть пустыми");
+        try {
+            if (left == null || right == null) {
+                throw new NullArgument("Операнды не должны быть пустыми");
+            }
+        } catch (NullArgument e) {
+            System.out.println(e.getMessage());
+            ;
         }
         this.left = left;
         this.right = right;
@@ -35,13 +43,19 @@ public class BinaryOperation extends Expression {
     public double evaluate() {
         double l = left.evaluate();
         double r = right.evaluate();
-        return switch (op) {
-            case PLUS -> l + r;
-            case MINUS -> l - r;
-            case DIV -> l / r;
-            case MUL -> l * r;
-            default -> throw new UnsupportedOperationException("Неизвестный оператор: " + (char) op);
-        };
+        double result = 0;
+        try {
+            result = switch (op) {
+                case PLUS -> l + r;
+                case MINUS -> l - r;
+                case DIV -> l / r;
+                case MUL -> l * r;
+                default -> throw new UnsupportedOperation("Неизвестный оператор: " + (char) op);
+            };
+        } catch (UnsupportedOperation e) {
+            System.out.println(e.getMessage());
+        }
+        return result;
     }
 
     @Override
